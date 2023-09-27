7a9c6175c3bcc6aa459fd0167306f25f40ce5b39
datwatshesed <91712002+oolteem8@users.noreply.github.com>
2023-09-27 16:03:18 +0800

    Started Tutorial on React

---
+++ script.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+var navbar = (
+    <nav>
+        <h1>oolteem8</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
