f42ce7abd60b31c001718cb9b6c98da2e8c8b37c
datwatshesed <91712002+oolteem8@users.noreply.github.com>
2023-09-28 16:20:10 +0800

    Misc Commit

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function Header() {
     return (
         <header>
             <nav>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" width={50}/>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" width="50"/>
             </nav>
         </header>
     )
