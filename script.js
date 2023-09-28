e2fc4650c8b2b391c95b8fedf82ef6ae8ad9eca2
datwatshesed <91712002+oolteem8@users.noreply.github.com>
2023-09-28 16:35:41 +0800

    Upgraded Fun Facts Webpage

--- script.js
+++ script.js
@@ -15,8 +15,44 @@ var navbar = (
     </div>
 )
 
+function Navbar() {
+    return (
+        <header>
+            <nav style={{display: "flex", alignItems: "center"}}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" 
+                     style={{display: "inline-block", paddingRight: "220px"}} width="50px" />
+
+                <ul style={{display: "inline-block", paddingLeft: "5px"}}>
+                    <li style={{display: "inline-block", marginRight: "10px"}}>Pricing</li>
+                    <li style={{display: "inline-block", marginRight: "10px"}}>About</li>
+                    <li style={{display: "inline-block", marginRight: "10px"}}>Contact</li>
+                </ul>
+
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    navbar
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>
 );
