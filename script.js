7a5b2b635cec4cbe53a8228d2ef033d107c6d741
datwatshesed <91712002+oolteem8@users.noreply.github.com>
2023-09-28 14:56:34 +0800

    React Fun Facts

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-import React from "react"
+// import React from "./react-app/node_modules/react"
+// import ReactDOM from './react-app/node_modules/react-dom/client';
 
 var navbar = (
-    <nav>
-        <h1>oolteem8</h1>
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" width={50}></img>
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
 
-ReactDOM.render(navbar, document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    navbar
+);
